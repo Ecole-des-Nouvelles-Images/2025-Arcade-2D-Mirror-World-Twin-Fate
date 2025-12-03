91e0270f809d015beb47feb2a795853fb590cc9b
Drayzzer <j.naval@nouvellesimages.xyz>
2025-12-03 09:56:31 +0100

    add separate player

--- Assets/__Workspaces/Jordan/Script/Ennemy/EnnemiesLife.cs
+++ Assets/__Workspaces/Jordan/Script/Ennemy/EnnemiesLife.cs
@@ -1,4 +1,5 @@
 using System;
+using __Workspaces.Jordan.Script.Player;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -12,7 +13,7 @@ namespace __Workspaces.Jordan.Script.Ennemy
 
     public class Ennemy : MonoBehaviour
     {
-        [SerializeField] private int maxHealth = 3;
+        [SerializeField] private int _maxHealth = 3;
         [SerializeField] private ColorType enemyColor; // Choisir Red ou Blue dans l’inspecteur
         public int currentHealth;
         
@@ -20,6 +21,7 @@ namespace __Workspaces.Jordan.Script.Ennemy
 
         public void TakeDamage(int damage)
         {
+            Debug.Log("I'm taken damage " + damage);
             currentHealth -= damage;
             if (currentHealth <= 0)
             {
@@ -28,23 +30,25 @@ namespace __Workspaces.Jordan.Script.Ennemy
         }
         private void Start()
         {
-            currentHealth =  maxHealth;
+            currentHealth =  _maxHealth;
         } 
         private void OnCollisionEnter2D(Collision2D collision)
         {
             Debug.Log("ca collisionne");
-                //  Ennemi Bleu
+            
+                //  Ennemi 
+                Debug.Log(" La Couleur de l'ennemi est " +enemyColor + "Le Tag de la collision est "+ collision.gameObject.tag);
                 if (enemyColor == ColorType.Blue && collision.collider.CompareTag("BulletBlue"))
                 {
-                    TakeDamage(1);
-                    Destroy(gameObject);
+                    BulletScript bullet = collision.collider.GetComponent<BulletScript>();
+                    TakeDamage(bullet.Damage);
                 }
 
                 // Ennemi Rouge
                 if (enemyColor == ColorType.Red && collision.collider.CompareTag("BulletRed"))
                 {
-                    TakeDamage(1);
-                    Destroy(gameObject);
+                    BulletScript bullet = collision.collider.GetComponent<BulletScript>();
+                    TakeDamage(bullet.Damage);
                 }
         }
     }
