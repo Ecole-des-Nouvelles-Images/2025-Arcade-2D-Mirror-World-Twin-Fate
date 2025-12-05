35d9e56c2da5bd582946fb6537ee58cd146e2704
Yo-Han Floch <y.floch@ensi.lan>
2025-12-05 15:02:49 +0100

    Eye Dragon follow

---
+++ Assets/__Workspaces/Yo-Han/Script/eyefollowBlue.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class eyefollowBlue: MonoBehaviour
+{
+    public float maxDistance = 0.07f; 
+
+    private Transform player;
+    private Vector3 startLocalPos;
+
+    void Start()
+    {
+        GameObject p = GameObject.FindGameObjectWithTag("PlayerBlue");
+        if (p != null)
+            player = p.transform;
+
+        startLocalPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if (player == null) return;
+        
+        Vector2 dir = (player.position - transform.parent.position).normalized;
+        
+        transform.localPosition = startLocalPos + (Vector3)dir * maxDistance;
+    }
+}
