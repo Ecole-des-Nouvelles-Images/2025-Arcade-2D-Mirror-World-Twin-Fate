8b73724f522145aa356b6344353ee92eca7f403f
Baptiste Gelly <gellybaptiste@gmail.com>
2025-12-12 13:09:52 +0100

    sound

---
+++ Assets/__Workspaces/Baptiste/scripts/test_son.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class test_son : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] randomSoundFX;
+    
+    AudioSource audioSource;
+}
