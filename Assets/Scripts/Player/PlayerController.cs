c327a07cb8104aaa3485788ca89166afa4dbe0f5
Alexis Taverdet <a.taverdet@ensi.lan>
2025-12-16 17:01:19 +0100

    Oui

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -27,6 +27,8 @@ namespace Player
         private Rigidbody2D _rb;
         public bool IsCharging = false;
         public float chargeTime;
+        public float shootCooldown = 0.3f;
+        private float timer = 0f;
         public float chargeThreshold = 0.4f;
         public float _minChargeTime = 1.5f;
         public int _chargeMultiplier;
@@ -41,8 +43,9 @@ namespace Player
 
         private void Update()
         {
-            animator.SetFloat("xVelocity", _rb.linearVelocity.x);
+            timer += Time.deltaTime;
             _rb.linearVelocity = new Vector2(Move.x * _speed, Move.y * _speed);
+            animator.SetFloat("xVelocity", _rb.linearVelocity.x);
           
             if (IsCharging)
             {
@@ -57,8 +60,12 @@ namespace Player
 
         private void DoFire()
         {
-            GameObject instantiate = Instantiate(_prfBullet, transform.position, Quaternion.identity);
-            instantiate.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _bulletspeed);
+            if (timer >= shootCooldown)
+            {
+                GameObject instantiate = Instantiate(_prfBullet, transform.position, Quaternion.identity);
+                instantiate.GetComponent<Rigidbody2D>().AddForce(Vector2.up * _bulletspeed);
+                timer = 0f;
+            }
         }
 
         private void StartFire()
